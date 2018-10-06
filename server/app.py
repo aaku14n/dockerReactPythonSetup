f1bc04caa43b96b989ea89baff82f91eea916f83
y-aakarsh <aakarsh@xelpmoc.in>
2018-10-06 11:55:43 +0530

    ADding changes on final setup for docker

--- server/app.py
+++ server/app.py
@@ -1,9 +1,14 @@
-From flask import Flask
+import time
+
+from flask import Flask
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+@app.route('/')
+def hello():
+    return 'Hello World! I have been seen {} times.\n'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True,port="5000")
